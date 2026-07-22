8db7ef3fd902c2d707c31fc0e87435c0959034f9
dilusha-ola <dilushaolaboduwa3@gmail.com>
2026-07-22 13:35:22 +0530

    remove duplicated api

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from app.api.v1 import bot
+from app.api.v1 import chat
 
 api_router = APIRouter()
-api_router.include_router(bot.router)
+api_router.include_router(chat.router)
