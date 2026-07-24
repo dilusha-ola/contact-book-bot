43bc1c082018784fe552fbf8850a61c9aafb071a
dilusha-ola <dilushaolaboduwa3@gmail.com>
2026-07-24 17:52:55 +0530

    update config.py ,hide secret data

--- app/core/config.py
+++ app/core/config.py
@@ -5,19 +5,19 @@ class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
     PORT: int = 8001
     PLATFORM_API_URL: str = "http://localhost:8000/api/v1"
-    SECRET_KEY: str = "bot_testing_secret_key_12345"
+    SECRET_KEY: str = ""
     ENVIRONMENT: str = "development"
-    MONGODB_URL: str = "mongodb+srv://dilushaolaboduwa3_db_user:LJ06yJuoG5wnNpIQ@cluster0.mcyxj4n.mongodb.net/?appName=Cluster0"
+    MONGODB_URL: str = ""
 
     # LLM Settings
     LLM_PROVIDER: str = "groq"
     LLM_MODEL: str = "llama-3.3-70b-versatile"
-    GROQ_API_KEY: str = "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"
+    GROQ_API_KEY: str = ""
     OPENAI_API_KEY: str = ""
     GOOGLE_API_KEY: str = ""
 
     class Config:
         env_file = ".env"
-        case_sensitive = True
+        extra = "ignore"
 
 settings = Settings()
